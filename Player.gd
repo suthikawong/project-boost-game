3486a0ee8dd2364eae5ef2b7bf5d7ee755c6bc1f
suthikawong <suthika.wsrd@gmail.com>
2024-01-21 15:33:44 +0700

    add audio

--- Player.gd
+++ Player.gd
@@ -2,15 +2,20 @@ extends RigidBody3D
 
 ## How much vertical forge to apply when moving.
 @export_range(750.0, 3000.0) var thrust: float = 1000.0
-
 @export var torque_thrust: float = 100.0
-
 var is_transitioning: bool = false
+@onready var explosion_audio: AudioStreamPlayer = $ExplosionAudio
+@onready var success_audio: AudioStreamPlayer = $SuccessAudio
+@onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * thrust)
+		if rocket_audio.playing == false:
+			rocket_audio.play()
+	else:
+		rocket_audio.stop()
 		
 	if Input.is_action_pressed("rotate_left"):
 		apply_torque(Vector3(0.0, 0.0, torque_thrust) * delta)
@@ -20,24 +25,28 @@ func _process(delta: float) -> void:
 
 # จะเข้าฟังก์ชันนี้เมื่อ player ไปแตะกับ object อื่นๆ
 func _on_body_entered(body: Node) -> void:
-	if is_transitioning == false:
+	if is_transitioning == false: # ถ้ากำลังเปลี่ยนฉากอยู่จะไม่เข้า
 		if "Goal" in body.get_groups():
 			complete_level(body.file_path)
 		if "Hazard" in body.get_groups():
 			crash_sequence()
 
 func crash_sequence() -> void:
+	rocket_audio.stop()
+	explosion_audio.play()
 	set_process(false)
 	is_transitioning = true
 	var tween = create_tween()
-	tween.tween_interval(1.0)
+	tween.tween_interval(2.5)
 	tween.tween_callback(get_tree().reload_current_scene)
 
 func complete_level(next_level_file: String) -> void:
+	rocket_audio.stop()
+	success_audio.play()
 	set_process(false)
 	is_transitioning = true
 	var tween = create_tween()
-	tween.tween_interval(1.0)
+	tween.tween_interval(2.0)
 	tween.tween_callback(
 		get_tree().change_scene_to_file.bind(next_level_file)
 	)
